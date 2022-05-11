fdf7ea8b278b514683b19a1cca794f688203a721
Maks <maksx_x_x@mail.ru>
2022-05-11 15:21:36 +0700

    Программа которая выполняет задачу (Код)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача. Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+
+string[] strArray = { "das", "548", "retry", "=-)","привет", "Вау" };
+
+PrintArray();
+Console.WriteLine("Массив из строк, длинна которых меньше либо равна 3 символа");
+SortThreeElrments();
+
+//  Вывод исходного массива.
+void PrintArray()
+{
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        Console.Write(strArray[i] + " ");
+    }
+    Console.WriteLine();
+}
+// Нахождение масива строк, длинна которых меньше либо равна 3. Вывод искомого массива. 
+void SortThreeElrments()
+{
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        if (strArray[i].Length <=3) 
+        Console.Write(strArray[i] + " ");
+    }
+    Console.WriteLine();
+}
